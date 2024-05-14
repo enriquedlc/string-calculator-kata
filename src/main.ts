66514525449198091c40e19f76b3034b7249d016
enrique <enriquedlc.dev@gmail.com>
2024-05-14 17:54:18 +0100

    refactor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ function sum(a: number, b: number) {
   return a + b
 }
 
+function parse(input: string) {
+  const delimiter = ","
+  const body = input
+  return [delimiter, body] as const
+}
+
 export function add(numbers: string): number {
-  return numbers.split("\n").join(",").split(",").map(Number).reduce(sum, 0)
+  const [delimiter, body] = parse(numbers)
+  return body.split("\n").join(delimiter).split(delimiter).map(Number).reduce(sum, 0)
 }
